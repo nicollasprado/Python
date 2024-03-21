244dada338538dd4af0b4e413fc10a39ee5d8462
Nicollas Prado <127448248+nicollasprado@users.noreply.github.com>
2024-03-21 10:31:38 -0300

    lista 4 codeforces

--- exercicios/codeforces/lista_4/triangulo.py
+++ exercicios/codeforces/lista_4/triangulo.py
@@ -1,22 +1,29 @@
-def possoCriarUmTriangulo(varetas: list):
-    minIndex = 0
-    maxIndex = 2
-    proxVaretaIndex = maxIndex + 1
+def possoFormarTriangulo(varetas: list):
     resultado = 'N'
-    while (maxIndex < 4):
-        if maxIndex == 4:
-            proxVaretaIndex = 3
-        somaDoisPrimeirosVal = sum(varetas[minIndex:maxIndex])
-        if somaDoisPrimeirosVal > varetas[proxVaretaIndex]:
-            resultado = 'S'
-            break
+    index = 1
+    for vareta in varetas:
+        index1 = index + 1
+        index2 = index + 2
+        if (index1 == 4):
+            index1 = 0
+        if (index2 == 5):
+            index2 = 1
+        elif (index2 == 4):
+            index2 = 0
         else:
-            minIndex += 1
-            maxIndex += 1
+            if (vareta + varetas[index] > varetas[index1]) and (vareta + varetas[index1] > varetas[index]) and (varetas[index] + varetas[index1] > vareta):
+                resultado = 'S'
+                break
+            elif (vareta + varetas[index] > varetas[index2]) and (vareta + varetas[index2] > varetas[index]) and (varetas[index] + varetas[index2] > vareta):
+                resultado = 'S'
+                break
+            else:
+                index += 1
+                continue
     return resultado
+            
     
-
 vareta1, vareta2, vareta3, vareta4 = map(int, input().split())
 varetas = [vareta1, vareta2, vareta3, vareta4]
 
-print(possoCriarUmTriangulo(sorted(varetas)))
+print(possoFormarTriangulo(varetas))
