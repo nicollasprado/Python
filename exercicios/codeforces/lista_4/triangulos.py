4846dbbebda6105f14c965aa80e37924c6b653b1
Nicollas Prado <127448248+nicollasprado@users.noreply.github.com>
2024-03-25 10:46:49 -0300

    codeforces lista 4

--- exercicios/codeforces/lista_4/triangulos.py
+++ exercicios/codeforces/lista_4/triangulos.py
@@ -1,18 +1,27 @@
-def getTriangleType(sticks: list):
-
-    return sticksRad
+def getTriangleType(stick1, stick2, stick3):
+    triangleType = ""
+    calculation = (stick1 ** 2) + (stick2 ** 2) - (stick3 ** 2)
+    if (calculation > 0):
+        # acutangulo
+        triangleType = "a"
+    elif (calculation == 0):
+        # retangulo
+        triangleType = "r"
+    elif (calculation < 0):
+        # obtusangulo
+        triangleType = "o"
+    return triangleType
 
 def canFormTriangle(stick1, stick2, stick3):
     if (stick1 + stick2 > stick3) and (stick1 + stick3 > stick2) and (stick2 + stick2 > stick1):
         return True
 
 stick1, stick2, stick3 = map(int, input().split())
-sticks = [stick1, stick2, stick3]
 
 boolCanForm = canFormTriangle(stick1, stick2, stick3)
 if (boolCanForm == True):
-    print(getTriangleType(sticks))
-    print(getLongestStick(sticks))
-    print(getMediumStick(sticks))
+    print(getTriangleType(stick1, stick2, stick3))
+else:
+    print("n")
 
-    # https://www.youtube.com/watch?v=tmY8VNp1gWk
+    # Arrumar
