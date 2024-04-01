12b7800fd1de38a5af23503e95809eb528e80cfb
Nicollas Prado <nicollasprado.profissional@gmail.com>
2024-03-31 23:01:21 -0300

    Atualização da questão Nota cortada da lista 4 do codeforces

--- exercicios/codeforces/lista_4/nota-cortada.py
+++ exercicios/codeforces/lista_4/nota-cortada.py
@@ -1,58 +1,27 @@
 # se um dos pedaços possuir estritamente mais da metade da área da nota original, então ele vale 100 reais
 lengthBill, heighBill = 160, 70
 areaBill = lengthBill * heighBill
+halfBill = areaBill / 2
 
-def whoGotHundred(baseCut, topCut):
+def whoGotHundred(cuttedArea):
     # 1 == Felix, 2 == Mariza
-    whoGot = 0
-    if ((80 - baseCut) == (topCut - 80)):
+    whoGot = 1
+    if (cuttedArea == halfBill):
         whoGot = 0
-    elif (baseCut == 0) and (topCut == 0):
-        whoGot = 0
-
-    elif (baseCut <= 80) and (topCut <= 80):
+    elif (halfBill > cuttedArea):
         whoGot = 2
-    elif (baseCut >= 80) and (topCut >= 80):
-        whoGot = 1
-
-    elif (baseCut < 80) and (topCut > 80):
-        if ((80 - baseCut) > (topCut - 80)):
-            whoGot = 2
-        elif ((80 - baseCut) < (topCut - 80)):
-            whoGot = 1
-
-    elif (baseCut > 80) and (topCut < 80):
-        if ((baseCut - 80) > (80 - topCut)):
-            whoGot = 1
-        elif ((baseCut - 80) < (80 - topCut)):
-            whoGot = 2
     return whoGot
 
-def getCuttedArea(baseCut, topCut):
-    if (topCut > baseCut):
-        rectanglePart = baseCut * 70
-        trianglePart = (topCut - baseCut) * 70
-        totalCuttedArea = rectanglePart + trianglePart
-    elif (baseCut > topCut):
-        rectanglePart = topCut * 70
-        trianglePart = (baseCut - topCut) * 70
-        totalCuttedArea = rectanglePart + trianglePart
-    return totalCuttedArea
-
-def verifyCuttedArea(cuttedArea):
-    halfBill = areaBill / 2
-    if (cuttedArea > halfBill):
+def verifyCuttedArea(baseCut, topCut):
+    if (baseCut <= 0) and (topCut <= 0) or (baseCut > 160) or (topCut > 160):
         return True
+
 
 baseCutPointToLeft = int(input())
 topCutPointToLeft = int(input())
+cuttedArea = ((baseCutPointToLeft + topCutPointToLeft) * 70) / 2
 
-if (baseCutPointToLeft <= 0) and (topCutPointToLeft <= 0) or (baseCutPointToLeft > 160) or (topCutPointToLeft > 160):
-    # raise Exception("Os dois valores não podem ser zero")
-    print(0)
+if (verifyCuttedArea(baseCutPointToLeft, topCutPointToLeft) != True):
+    print(whoGotHundred(cuttedArea))
 else:
-    cuttedArea = getCuttedArea(baseCutPointToLeft, topCutPointToLeft)
-    if (verifyCuttedArea(cuttedArea) == True):
-        print(whoGotHundred(baseCutPointToLeft, topCutPointToLeft))
-    else:
-        print(0)
+    print(0)
