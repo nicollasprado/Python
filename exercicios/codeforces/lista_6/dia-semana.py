3c64d5153fe994193356c4afb6dae317f7435f2f
nicollasprado <nicollasprado.profissional@gmail.com>
2024-07-04 10:31:32 -0300

    codeforces lista 6

---
+++ exercicios/codeforces/lista_6/dia-semana.py
@@ -0,0 +1,11 @@
+def dia_da_semana(h, d):
+    h = {
+        "Domingo": 1,
+        "Segunda-feira": 2,
+        "Terca-feira": 3,
+        "Quarta-feira": 4,
+        "Quinta-feira": 5,
+        "Sexta-feira": 6,
+        "Sabado": 7
+        }
+    
