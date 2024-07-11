a8b8967b998d175ae2d715dfeb09cf56e7803d21
Nicollas Matheus Prado Pinheiro <20241014040014@ifrn.local>
2024-07-11 10:30:41 -0300

    inicio da lista de recursividade

---
+++ exercicios/recursividade-lista1/q1.py
@@ -0,0 +1,15 @@
+def calcular_divisores(n, div):
+    if(div == 1):
+        print(f"divisor encontrado: {div}" )
+        return 1
+    else:
+        if(n % div == 0):
+            print(f"divisor encontrado: {div}" )
+            return 1 + calcular_divisores(n, div-1)
+        else:
+            return 0 + calcular_divisores(n, div-1)
+        
+def conta_divisores(n):
+    return calcular_divisores(n, n)
+    
+print(conta_divisores(7))
---
+++ exercicios/recursividade-lista1/q4.py
@@ -0,0 +1,16 @@
+def contadorAlgarismos(n, qtdAlg):
+    if(n % 10 == 0):
+        contador = 0+qtdAlg
+        return contador
+    else:
+        return contadorAlgarismos(n//10, qtdAlg+1)
+
+
+def conta_algarismos(n):
+    return contadorAlgarismos(n, 0)
+
+num = 1255312312931329
+print(conta_algarismos(num))
+
+certo = len(str(num))
+print(f"certo: {certo}")
